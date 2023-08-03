3ad17aba5ceef3a78b89ac79109944a9ef42e642
sandeepmallina <mallinasandeep21@gmail.com>
2023-08-04 00:15:47 +0530

    Added some padding and margin to Abut and Portfolio

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -7,12 +7,12 @@ const About = () => {
       className="w-full h-screen bg-gradient-to-b from-gray-800 to-black text-white  "
     >
       <div className="max-w-screen-lg p-4 mx-auto flex flex-col justify-center w-full h-full">
-        <div className="pt-10 mt-24 md:pt-14">
+        <div className="pt-14 mt-24 md:pt-14">
           <p className="text-4xl font-bold inline border-b-4 border-gray-500 ">
             About
           </p>
         </div>
-        <p className="text-xl mt-20">
+        <p className="text-xl mt-10">
           I'm a passionate and skilled web developer with a solid grasp of core
           concepts such as React JS, Tailwind, MongoDB and Firebase. I can
           create stunning websites that will make your eyes pop out of your
